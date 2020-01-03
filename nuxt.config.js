344765c50c87bf0d03023aced880027fdc154a1b
Takahiro Hatanaka <i@takahirohatanaka.com>
2020-01-03 22:54:09 +0900

    add files

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Takahiro Hatanaka',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'keywords', content: 'takahirohatanaka,畑中貴弘' },
+      { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/main.scss',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/fontawesome.ts',
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxt/typescript-build',
+
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/style-resources',
+  ],
+
+  styleResources: {
+    scss: [
+      '@/assets/css/variables.scss',
+    ]
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <header>
+      <img class="my-icon" src="icon.png" />
+      <h1>Takahiro Hatanaka</h1>
+    </header>
+
+    <article>
+      <section class="about-me">
+        <h1>About me</h1>
+        <div>
+          I am Takahiro Hatanaka. I am working as a IT engineer in Tokyo, Japan
+          since 2011.
+        </div>
+      </section>
+
+      <skills />
+
+      <section class="contacts">
+        <h1>Contacts</h1>
+        <div>
+          <ul>
+            <li>
+              <font-awesome-icon :icon="['fab', 'github']" class="icon" />
+              <a href="https://github.com/takahirohatanaka"
+                >github.com/takahirohatanaka</a
+              >
+            </li>
+
+            <li>
+              <font-awesome-icon :icon="['fab', 'flickr']" class="icon" />
+              <a href="https://www.flickr.com/photos/takahirohatanaka/"
+                >flickr.com/takahirohatanaka</a
+              >
+            </li>
+
+            <li>
+              <img class="icon" src="/qiita.png" />
+              <a href="https://qiita.com/takahirohatanaka"
+                >qiita.com/takahirohatanaka</a
+              >
+            </li>
+
+            <li>
+              <font-awesome-icon :icon="['far', 'envelope']" class="icon" />
+              i<font-awesome-icon
+                icon="at"
+                style="width: 1rem; margin: auto .1rem;"
+              />takahirohatanaka.com
+            </li>
+          </ul>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<script language="ts">
+import Skills from "~/components/skills.vue";
+
+export default {
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  margin-bottom: 2rem;
+  vertical-align: middle;
+
+  .my-icon {
+    border-radius: 50%;
+    height: 50px;
+    float: left;
+    margin-right: 1rem;
+  }
+
+  h1 {
+    font-size: 45px;
+    text-transform: uppercase;
+  }
+}
+
+.contacts {
+  ul {
+    list-style: none;
+    padding-left: 1rem;
+
+    li {
+      margin-bottom: 0.5rem;
+      display: flex;
+
+      .icon {
+        font-size: 1.6rem;
+        margin-right: 0.5rem;
+        width: 1.4rem;
+      }
+    }
+  }
+}
+</style>
