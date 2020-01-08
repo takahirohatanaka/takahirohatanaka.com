4f32a40972e70e4d23846faea867df342fabf78c
Takahiro Hatanaka <i@takahirohatanaka.com>
2020-01-08 19:54:53 +0900

    component わけた

---
+++ components/contact.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="contacts">
+    <h1>Contacts</h1>
+    <div>
+      <ul>
+        <li>
+          <font-awesome-icon :icon="['fab', 'github']" class="icon" />
+          <a href="https://github.com/takahirohatanaka"
+            >github.com/takahirohatanaka</a
+          >
+        </li>
+
+        <li>
+          <font-awesome-icon :icon="['fab', 'flickr']" class="icon" />
+          <a href="https://www.flickr.com/photos/takahirohatanaka/"
+            >flickr.com/takahirohatanaka</a
+          >
+        </li>
+
+        <li>
+          <img class="icon" src="/qiita.png" />
+          <a href="https://qiita.com/takahirohatanaka"
+            >qiita.com/takahirohatanaka</a
+          >
+        </li>
+
+        <li>
+          <font-awesome-icon :icon="['far', 'envelope']" class="icon" />
+          i<font-awesome-icon
+            icon="at"
+            style="width: 1rem; margin: auto .1rem;"
+          />takahirohatanaka.com
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.contacts {
+  ul {
+    list-style: none;
+    padding-left: 1rem;
+
+    li {
+      margin-bottom: 0.5rem;
+
+      svg {
+        display: inline-block;
+      }
+
+      .icon {
+        display: inline-block;
+        font-size: 1.6rem;
+        margin-right: 0.5rem;
+        width: 1.4rem;
+        height: 1.4rem;
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,52 +15,19 @@
       </section>
 
       <skills />
-
-      <section class="contacts">
-        <h1>Contacts</h1>
-        <div>
-          <ul>
-            <li>
-              <font-awesome-icon :icon="['fab', 'github']" class="icon" />
-              <a href="https://github.com/takahirohatanaka"
-                >github.com/takahirohatanaka</a
-              >
-            </li>
-
-            <li>
-              <font-awesome-icon :icon="['fab', 'flickr']" class="icon" />
-              <a href="https://www.flickr.com/photos/takahirohatanaka/"
-                >flickr.com/takahirohatanaka</a
-              >
-            </li>
-
-            <li>
-              <img class="icon" src="/qiita.png" />
-              <a href="https://qiita.com/takahirohatanaka"
-                >qiita.com/takahirohatanaka</a
-              >
-            </li>
-
-            <li>
-              <font-awesome-icon :icon="['far', 'envelope']" class="icon" />
-              i<font-awesome-icon
-                icon="at"
-                style="width: 1rem; margin: auto .1rem;"
-              />takahirohatanaka.com
-            </li>
-          </ul>
-        </div>
-      </section>
+      <contact />
     </article>
   </div>
 </template>
 
 <script language="ts">
-import Skills from "~/components/skills.vue";
+import Skills from "@/components/skills";
+import Contact from "@/components/contact";
 
 export default {
   components: {
-    Skills
+    Skills,
+    Contact
   }
 };
 </script>
@@ -80,29 +47,6 @@ header {
   h1 {
     font-size: 45px;
     text-transform: uppercase;
-  }
-}
-
-.contacts {
-  ul {
-    list-style: none;
-    padding-left: 1rem;
-
-    li {
-      margin-bottom: 0.5rem;
-
-      svg {
-        display: inline-block;
-      }
-
-      .icon {
-        display: inline-block;
-        font-size: 1.6rem;
-        margin-right: 0.5rem;
-        width: 1.4rem;
-        height: 1.4rem;
-      }
-    }
   }
 }
 </style>
