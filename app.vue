fa3232b0404f56c91df7de6d27e796b8cb764e61
Takahiro Hatanaka <takahiro.hatanaka@stanby.inc>
2022-02-15 11:33:52 +0900

    add gtm

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="root" class="text-base w-screen">
-    <div class="mx-auto my-10 lg:max-w-screen-lg max-w-xs text-gray-600">
+    <div class="mx-auto my-10 md:max-w-screen-md max-w-xs text-gray-600">
       <TheTitle class="mb-7" />
       <About class="mb-4" />
       <SkillList class="mb-4" />
