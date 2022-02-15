dfbdc3b64621b674e4625b84a52c6cf4e104f383
Takahiro Hatanaka <i@takahirohatanaka.com>
2022-02-15 16:28:08 +0900

    Update spacing

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,6 +3,11 @@
     <section>
       <h1 class="text-2xl uppercase">Contact</h1>
       <div class="my-2">
+        <p class="mb-2">
+          This page is in English, but I don't actually speak English. It would
+          be great if you could contact me in Japanese.
+        </p>
+
         <ul>
           <li class="flex items-center mb-1">
             <span class="material-icons mr-2">code</span>
