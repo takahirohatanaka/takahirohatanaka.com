e948040857a8d17c916da0bf36c79afb9b3bba5f
Takahiro Hatanaka <i@takahirohatanaka.com>
2023-01-09 22:47:22 +0900

    Update to Nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,30 @@
-import { defineNuxtConfig } from "nuxt";
-
 export default defineNuxtConfig({
-  meta: {
-    title: "Takahiro Hatanaka",
-    meta: [
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        name: "keywords",
-        content:
-          "takahirohatanaka,takahiro,hatanaka,畑中貴弘,畑中,貴弘,はたなかたかひろ,はたなか,たかひろ",
-      },
-      { "http-equiv": "x-ua-compatible", content: "ie=edge" },
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        href: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined",
-        rel: "stylesheet",
-      },
-    ],
+  app: {
+    head: {
+      title: "Takahiro Hatanaka",
+      meta: [
+        {name: "viewport", content: "width=device-width, initial-scale=1"},
+        {
+          name: "keywords",
+          content:
+            "takahirohatanaka,takahiro,hatanaka,畑中貴弘,畑中,貴弘,はたなかたかひろ,はたなか,たかひろ",
+        },
+        {"http-equiv": "x-ua-compatible", content: "ie=edge"},
+      ],
+      link: [
+        {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
+        {
+          href: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined",
+          rel: "stylesheet",
+        },
+      ],
+    },
   },
   css: ["@/assets/css/fonts.css", "@/assets/css/tailwind.css"],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
 });
